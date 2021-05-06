a5141bf2a322e9a8de68d45f2d5a590104caab87
Dmitriy Lutsishin <graf.luck@gmail.com>
2021-05-06 17:19:16 +0300

    Доделал модальные окна и фоны на index

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -2,6 +2,11 @@ const navigationToggle = document.querySelector(".navigation__toggle");
 const navigationList = document.querySelector(".navigation");
 const pageHeader = document.querySelector(".page-header");
 
+navigationList.classList.remove("navigation--opened");
+navigationList.classList.add("navigation--closed");
+pageHeader.classList.add("page-header--navigation-closed");
+navigationToggle.classList.remove("navigation__toggle--disabled");
+
 navigationToggle.addEventListener("click", function (evt) {
   if (navigationList.classList.contains("navigation--closed")) {
     evt.preventDefault();
